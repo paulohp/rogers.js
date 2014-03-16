8127d93561b3e3be7ee3312d2824ca0c218739cf
Paulo Pires <taralloqueiroiz@gmail.com>
2014-03-16 12:18:41 -0300

    refactor for use characters as a module

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-var request  = require('request'),
-    Promise = require('bluebird');
+function openConn(credentials){
+  return require('./lib')(credentials);
+}
 
-module.exports = function() {
-  var public_key  = "",
-      private_key = "",
-      characters  = {};
-
-    characters.findAll = function() {
-      request({
-        url:"http://gateway.marvel.com/v1/public/characters",
-        json:true,
-        qs:{
-          apikey:public_key
-        }
-      }, function(err, response){
-        if (err) {
-          return Promise.reject(err);
-        }
-        if (response.statusCode !== 200) {
-          return Promise.reject(new Error(response.body));
-        }
-
-        Promise.resolve(response.body);
-
-      });
-    },
-    characters.findByName = function() {
-
-    },
-    characters.find = function() {
-
-    };
-
-    return characters;
-};
+module.exports = openConn;
