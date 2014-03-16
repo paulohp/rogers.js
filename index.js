9e83906a34a048b56e646dd2313f4733eec65611
Paulo Pires <taralloqueiroiz@gmail.com>
2014-03-16 01:42:57 -0300

    init mockup of characters API

---
+++ index.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+module.exports = function() {
+  var public_key  = "",
+    private_key = "",
+    characters  = {};
+
+    characters.findAll = function() {
+
+    },
+    characters.findByName = function() {
+
+    },
+    characters.find = function() {
+
+    };
+};
